2b1b14a2255c8c73d26474f3f8f45bcc8a906b4d
viktor-105 <viktor-105@yandex.ru>
2022-11-13 21:55:51 +0300

    added js

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,4 +1,12 @@
 // Импорт функционала ==============================================================================================================================================================================================================================================================================================================================
 // import { isMobile } from "./functions.js";
 // import { formsModules } from "./forms/forms.js";
+const bascetBox = document.querySelector('.bascet__box');
 
+window.addEventListener("load", function (e) {
+    
+    bascetBox.addEventListener('click', function () {
+        bascetBox.classList.toggle("active");
+        
+    });
+});
